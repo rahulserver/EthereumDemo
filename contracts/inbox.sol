1e07898508ad0ae88fe0caea4ab4fc7d69126286
rahulserver <rahulserver@gmail.com>
2020-04-05 17:16:19 +0530

    add web3 and other dependencies add tests add compile code add contract code

--- contracts/inbox.sol
+++ contracts/inbox.sol
@@ -1,5 +1,13 @@
-pragma ^0.4.5;
+pragma solidity ^0.4.5;
 
 contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
     
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
 }
